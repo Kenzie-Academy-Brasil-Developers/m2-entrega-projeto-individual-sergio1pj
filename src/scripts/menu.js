7979704b21aa26323053590b181c32fa62d1786a
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-09-19 11:28:47 -0300

    hotfix: const em vez de let e usar pt-BR preferencialmente

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -2,20 +2,18 @@ import { Api } from "./api.js";
 const botaoModal = document.querySelectorAll("[data-modal-control]")
 botaoModal.forEach(element => {
     element.addEventListener('click', event => {
-        let value = element.getAttribute("data-modal-control")
-        document.getElementById(value).classList.remove('desligado')
-        document.getElementById(value).classList.remove('esconder-modal')
+        const valor = element.getAttribute("data-modal-control")
+        document.getElementById(valor).classList.remove('desligado')
+        document.getElementById(valor).classList.remove('esconder-modal')
     })
 });
 const botaoCloseModal = document.querySelectorAll("[data-close-control]")
 botaoCloseModal.forEach(element => {
     element.addEventListener('click', event => {
-
-        let value2 = element.getAttribute("data-close-control")
-        document.getElementById(value2).classList.toggle('esconder-modal')
-
+        const valor = element.getAttribute("data-close-control")
+        document.getElementById(valor).classList.toggle('esconder-modal')
         setTimeout(() => {
-            document.getElementById(value2).classList.toggle('desligado')
+            document.getElementById(valor).classList.toggle('desligado')
         }, 1000);
     })
 });
